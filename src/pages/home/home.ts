d1df04d15e8dac776db58b43e13120421f5aec75
Aniruth Kasthuri <a.kasthuri@IEEE.ORG>
2018-06-01 15:30:53 -0400

    A

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,24 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
-  }
 
+  slidester = [
+    {
+      title: "WOOOOOOOOOOOF",
+      description: "Welcome to this slide",
+      image: "https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg",
+    },
+    {
+      title: "What is Ionic?",
+      description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+      image: "assets/img/ica-slidebox-img-2.png",
+    },
+    {
+      title: "What is Ionic Cloud?",
+      description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+      image: "assets/img/ica-slidebox-img-3.png",
+    }
+  ];
 }
